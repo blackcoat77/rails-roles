35ba12b48e1a10225978217f8089ad4b57e9dd91
blackcoat77 <kimanovic@gmail.com>
2016-11-08 13:47:15 -0500

    Adds scaffold statuses, gem cancancan

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107192431) do
+ActiveRecord::Schema.define(version: 20161108183814) do
+
+  create_table "statuses", force: :cascade do |t|
+    t.text     "post"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
