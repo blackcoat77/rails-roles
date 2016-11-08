9b6d0d6462bd38d3964d5302ae7060b877512f16
blackcoat77 <kimanovic@gmail.com>
2016-11-08 15:51:02 -0500

    Adds: class Ability, roles to User model, two roles to Devises's registrations new page, routes, migration roles mask to User model

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,6 +1,7 @@
 class StatusesController < ApplicationController
 
   before_action :authenticate_user!
+  load_and_authorize_resource
   before_action :set_status, only: [:show, :edit, :update, :destroy]
 
   # GET /statuses
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
-
-
-  resources :statuses
   root 'home#index'
   devise_for :users
+  resources :statuses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
